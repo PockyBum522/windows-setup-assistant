c7e0bbd86b4b18cbb6b054d92498b607b50f99c0
DSikes <david@sikespc.com>
2022-05-23 14:19:30 -0400

    Cleaning up batch file, adding more echos, preparing for release

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # WindowsPostSetupAssistant
 GUI to easily create and save configuration/app install profiles that can quickly be applied to fresh installations of Windows. Everything you need to set up your workstation the way you want it after installing Windows!
+
+# Objectives
+You should be able to install Windows 10 or 11 and then:
+
+As soon as you finish setup and get to a desktop, you should be able to run the main batch file as Admin and:
+
+* Select a profile that you created previously using this application
+* Answer questions for anything you've marked optional all at the beginning
+
+* Let it run (It will handle rebooting and resuming as necessary.)
+
+* When it's finished, with no interaction on your part after the initial prompts stage, you should have:
+    * Windows completely up-to-date, if desired
+    * All applications you want, installed
+    * Your workstation almost completely configured:
+        * All Windows settings changed to match your chosen settings
+        * All application settings changed to match your chosen settings
--- RUN FIRST AS ADMIN.bat
+++ RUN FIRST AS ADMIN.bat
@@ -1,56 +1,106 @@
 @echo off
 
 IF %ERRORLEVEL% EQU 0 (
+   
+   echo ---------------------------------------------------
+   echo NOTICE: TEMPORARY MEASURES FOR RUNNING APPLICATION
+   echo ---------------------------------------------------
    echo.
+   echo In order to ensure that no user interaction is needed, we need to do a few things:
+   echo 1. UAC Needs to be set to "Never Notify"
+   echo 2. Automatic logon to your user account needs to be set so that reboots may take place with no interaction
+   echo.
+   echo Once the application has finished, you will be prompted to ask if 
+   echo you want to set UAC and automatic logon back to their default settings
+   echo.
+   echo Once you have read this, press any key to continue and 
+   echo this script will assist you with setting these settings...
+   echo.
+
+   pause
+
 ) ELSE (
 
-   echo.-------------------------------------------------------------
+   echo -------------------------------------------------------------
    echo ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
-   echo. -------------------------------------------------------------
-   echo. If you're seeing this, it means you don't have admin privileges!
-   echo. You will need to restart this program with Administrator privileges by right-clicking and select "Run As Administrator"
-   echo Press any key to leave this program. Make sure to Run As Administrator next time!
+   echo -------------------------------------------------------------
+   echo. 
+   echo If you're seeing this, it means you don't have admin privileges!
+   echo You will need to restart this program with Administrator privileges by right-clicking and selecting "Run As Administrator"
+   echo. 
+   echo Press any key to exit. Make sure to Run As Administrator next time!
+   echo. 
 
    pause
 
    EXIT /B 1   
 )
 
-ECHO "WARNING: UAC Must be set to not prompt for this to run unattended!"
+echo.
+echo Installing Chocolatey
+echo.
 
-ECHO "Installing Chocolatey"
 powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo.
+echo Setting Chocolatey confirmation prompts to not show
+echo.
 
 choco feature enable -n=allowGlobalConfirmation
 
-:: In case it was already installed, this will update it if necessary
+echo.
+echo If Chocolatey was previously installed, checking for updates
+echo.
+
 choco upgrade chocolatey
+
+echo.
+echo Installing latest Dot NET SDK
+echo.
 
 choco upgrade dotnet-sdk
 
-:: Bugfix
+:: Bugfix that takes care of certain observed instances where 
+:: dot net dependencies were potentially not installed properly
 choco install dotnet-sdk --force
 
-choco upgrade cs-script
+echo.
+echo Installing powershell core
+echo.
 
-choco upgrade NotePadPlusPlus
-
+:: Ensures we can run any necessary powershell commands with the latest version if necessary
 choco upgrade powershell-core --install-arguments='"ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 USE_MU=1 ENABLE_MU=1"'
 
-:: Since we just installed pwsh, it won't be usable until we refresh environment variables
-::
-:: RefreshEnv.cmd (From Chocolatey!)
-::
-:: Batch file to read environment variables from registry and
-:: set session variables to these values.
-::
-:: With this batch file, there should be no need to reload command
-:: environment every time you want environment changes to propagate
+echo.
+echo Refreshing environment variables for this shell instance
+echo.
 
 ::echo "RefreshEnv.cmd only works from cmd.exe, please install the Chocolatey Profile to take advantage of refreshenv from PowerShell"
-echo | set /p dummy="Refreshing environment variables from registry for cmd.exe. Please wait..."
+call :RefreshEnvironmentVariables
 
-goto main
+echo.
+echo Now that all dependencies are installed, running main application
+echo.
+
+:: %~dp0 is shorthand for full path script is currently running from
+:: Run Application Here
+
+echo. ------------------------------------------------------
+echo. APPLICATION HAS FINISHED CONFIGURATION OF THIS MACHINE
+echo. ------------------------------------------------------
+echo.
+echo. Your workstation is now configured per your chosen settings profile.
+echo.
+echo. You can press any key to exit.
+echo.
+
+pause
+
+EXIT /B 1   
+
+:: -------------------------------------------------------
+:: BELOW HERE IS ALL CODE FROM CHOCOLATEY'S RefreshEnv.cmd
+:: -------------------------------------------------------
 
 :: Set one environment variable from registry key
 :SetFromReg
@@ -70,7 +120,7 @@ goto main
     )
     goto :EOF
 
-:main
+:RefreshEnvironmentVariables
     echo/@echo off >"%TEMP%\_env.cmd"
 
     :: Slowly generating final file
@@ -103,8 +153,4 @@ goto main
     SET "PROCESSOR_ARCHITECTURE=%OriginalArchitecture%"
 
     echo | set /p dummy="Finished refreshing environtment variables."
-
-    ECHO "WARNING: UAC Must be set to not prompt for this to run unattended!"
-   
-    :: %~dp0 is shorthand for full path script is currently running from
-    :: Run Application Here
+    echo.

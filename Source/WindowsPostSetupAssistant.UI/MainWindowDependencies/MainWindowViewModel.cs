26d2316afd90ba57a4da69e19b9ea1f86f8af52f
DSikes <david@sikespc.com>
2022-05-25 13:08:08 -0400

    Set up and populated the List of Card in MainWindowViewModel

--- Source/WindowsPostSetupAssistant.UI/MainWindowDependencies/MainWindowViewModel.cs
+++ Source/WindowsPostSetupAssistant.UI/MainWindowDependencies/MainWindowViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using WindowsPostSetupAssistant.Core.Interfaces;
 using WindowsPostSetupAssistant.Core.Models;
 using WindowsPostSetupAssistant.Core.Models.ConfigurationActions;
@@ -19,21 +17,29 @@ public class MainWindowViewModel
 
     private void InitializeWindowsThemesCard()
     {
-        // var cardActions = new List<IConfigurationAction>();
-        //
-        // var firstAction = new BasicConfigurationAction()
-        // {
-        //     
-        // };
-        //
-        // cardActions.Add();
-        //
-        // Cards.Add(
-        //     new Card()
-        //     {
-        //         Title = "",
-        //         CardActions = 
-        //     }
-        // );
+        var cardActions = new List<IConfigurationAction>();
+        
+        cardActions.Add(
+            new BasicConfigurationAction() { Description = "Enable dark theme" });
+
+        cardActions.Add(
+            new BasicConfigurationAction() { Description = "Enable windows transparency" });
+
+        cardActions.Add(
+            new TextInputConfigurationAction() { Description = "Set accent color to (hex):" });
+        
+        cardActions.Add(
+            new TextInputConfigurationAction() { Description = "Set start menu color to (hex):" });
+        
+        cardActions.Add(
+            new TextInputConfigurationAction() { Description = "Set wallpaper to color to (path):" });
+        
+        var windowsThemeSettingsCard = new Card()
+        {
+            Title = "Windows 10 Theme Options",
+            CardActions = cardActions
+        };
+        
+        Cards.Add(windowsThemeSettingsCard);
     }
 }

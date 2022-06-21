5ec929ef840dd6b02c6d3828b06d4888e5741465
DSikes <david@sikespc.com>
2022-06-21 05:08:59 -0400

    Added ICommand implementation

---
+++ Source/WindowsPostSetupAssistant.UI/Commands/ParameterCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WindowsPostSetupAssistant.UI.Commands
+{
+    /// <summary>
+    /// Command for MVVM that takes in a parameter from the UI
+    /// </summary>
+    public class ParameterCommand : ICommand
+    {
+        private readonly Action<object> _exec;
+
+        /// <summary>
+        /// Constructor where we set the Action to execute when the command is called
+        /// </summary>
+        /// <param name="exec">Action to execute when the command is called</param>
+        public ParameterCommand(Action<object> exec)
+        {
+            _exec = exec;
+        }
+        
+        /// <summary>
+        /// Returns true if the command can execute currently, false otherwise 
+        /// </summary>
+        /// <param name="parameter">Required for ICommand, is discarded</param>
+        /// <returns>Always true</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the current command
+        /// </summary>
+        /// <param name="parameter">Passed parameter from UI</param>
+        public void Execute(object parameter)
+        {
+            _exec.Invoke(parameter);
+        }
+        
+#pragma warning disable 67
+        /// <summary>
+        /// Fires when CanExecute changes state
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+}
---
+++ Source/WindowsPostSetupAssistant.UI/Commands/SimpleCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WindowsPostSetupAssistant.UI.Commands
+{
+    /// <summary>
+    /// Command that does not take any parameter from the UI
+    /// </summary>
+    public class SimpleCommand : ICommand
+    {
+        private readonly Action _exec;
+
+        /// <summary>
+        /// Constructor in which we set the Action to execute when this command is called
+        /// </summary>
+        /// <param name="exec">Action to execute when this command is called</param>
+        public SimpleCommand(Action exec)
+        {
+            _exec = exec;
+        }
+        
+        /// <summary>
+        /// Returns true if the command can execute currently, false otherwise 
+        /// </summary>
+        /// <param name="parameter">Required for ICommand, is discarded</param>
+        /// <returns>Always true</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes this command's action when called
+        /// </summary>
+        /// <param name="parameter">Required for ICommand, is discarded</param>
+        public void Execute(object parameter)
+        {
+            _exec.Invoke();
+        }
+        
+#pragma warning disable 67
+        /// <summary>
+        /// Fires when CanExecute changes state
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+}

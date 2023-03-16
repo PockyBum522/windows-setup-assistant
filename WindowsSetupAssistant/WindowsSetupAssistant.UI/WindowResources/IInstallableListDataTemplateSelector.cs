4d302ceb8af3852011df2346cb59c88714890492
DSikes <david@sikespc.com>
2023-03-16 11:29:30 -0400

    Finished killing warnings

--- WindowsSetupAssistant/WindowsSetupAssistant.UI/WindowResources/IInstallableListDataTemplateSelector.cs
+++ WindowsSetupAssistant/WindowsSetupAssistant.UI/WindowResources/IInstallableListDataTemplateSelector.cs
@@ -4,22 +4,32 @@ using WindowsSetupAssistant.Core.Models.IInstallables;
 
 namespace WindowsSetupAssistant.UI.WindowResources;
 
+/// <summary>
+/// Selector to template separators differently from other IInstallables when displaying in listViews 
+/// </summary>
 public class IInstallableListDataTemplateSelector : DataTemplateSelector
 {
-    public DataTemplate SeparatorDisplayTemplate { get; set; }
-    public DataTemplate IInstallableDisplayTemplate { get; set; }
+    /// <summary>
+    /// DataTemplate to use when displaying selectors
+    /// </summary>
+    public DataTemplate SeparatorDisplayTemplate { get; set; } = new();
+    
+    /// <summary>
+    /// DataTemplate to use when displaying any other IInstallable
+    /// </summary>
+    public DataTemplate IInstallableDisplayTemplate { get; set; } = new();
 
+    /// <summary>
+    /// Does the actual template selecting
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="container"></param>
+    /// <returns></returns>
     public override DataTemplate SelectTemplate(object item, DependencyObject container)
     {
-        if (item is SeparatorForInstallersList)
-        {
-            return SeparatorDisplayTemplate;
-        }
-        else 
-        {
-            return IInstallableDisplayTemplate;
-        }
-
-        return base.SelectTemplate(item, container);
+        if (item is not SeparatorForInstallersList) return IInstallableDisplayTemplate;
+        
+        // Otherwise:
+        return SeparatorDisplayTemplate;
     }
 }
